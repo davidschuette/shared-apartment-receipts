852458f4d1e849d5e109d10e9828211ca6976a68
David Schütte <david.schuette@outlook.de>
2021-11-23 23:41:49 +0100

    Safety commit, check contents

--- apps/api/src/app/user/service/user.service.ts
+++ apps/api/src/app/user/service/user.service.ts
@@ -12,6 +12,10 @@ export class UserService {
   }
 
   create(): Promise<any> {
-    return this.userRepo.save([{ name: Date.now().toString() }])
+    return this.userRepo.save([
+      { id: '5b6014fb-2477-430b-b04e-0ad759314009', name: 'Juli' },
+      { id: '0b55f116-a080-4344-bd89-ca027fbb162a', name: 'Dave' },
+      { id: '2b09159b-3076-46b8-8a40-02195eee3312', name: 'Brian' },
+    ])
   }
 }
